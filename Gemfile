66ff502cded4f9e1cbd3d429945cf002cfcef6c1
Tonatiuh Nunez <tonatiuhnb@gmail.com>
2014-03-05 21:48:09 -0600

    first commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0.rc2'
+
+gem 'sqlite3'
+
+gem 'sass-rails', '~> 4.0.0.rc2'
+
+gem 'uglifier', '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'turbolinks'
+
+gem 'jbuilder', '~> 1.2'
+
+gem 'gumby-framework'
+
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PomodoroApp::Application.config.secret_key_base = '1f56491b384f88c2ca60bb22a3e9c4c9c9c8c54f08e4575788128ab2b4a55443bf0373457ad5354fa598848765e693b3d9fc6dd57b4d45f182a4167db6fb3090'
